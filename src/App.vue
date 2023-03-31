3304ffa06a9ca3b158bece71cc609a83ec45d560
NariNoodles <104844130+NaritahHamu@users.noreply.github.com>
2023-03-31 16:51:59 +0200

    Added Add/Delete/Select Priority/Strikeout

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,33 @@
   {id: 2, label: "Task 2", completed: true},
   {id: 3, label: "Task 1", completed: false}
 ])
+  const newItem = ref("")
+  const newItemPriority = ref("")
+  const saveItem = (high)=>{
+    if(high){
+      itemsHighPriority.value.push({
+      id:itemsHighPriority.value.length +1,
+      label: newItem.value,
+      completed: false
+    })
+      newItem.value = ""
+      newItemPriority.value = ""
+    }
+    else{
+      itemsNormalPriority.value.push({
+      id:itemsHighPriority.value.length +1,
+      label: newItem.value,
+      completed: false
+    })      
+      newItem.value = ""
+    }
+  }
+  const toggleCompleted = (items)=>{
+    items.completed = !items.completed
+  }
+  const deleteThis = (array,item)=>{
+    array.splice(item, 1);
+  }
 
 
 </script>
@@ -22,12 +49,19 @@
     <div class="inputField">
       <form 
         class="add-item-form"
+        v-on:submit.prevent="saveItem(newItemPriority)"
       >
       <input
+        v-model.number="newItem"
         type="text"
         placeholder="Add a task!"
       >
-      
+      <label>
+        <input
+          type="checkbox" v-model="newItemPriority"
+        >
+        Priority
+      </label>
       <button
         class="btn btn-primary"
       >
@@ -39,23 +73,39 @@
     <ul class="HighPriority">
       <li
         v-for="(item, index) in itemsHighPriority"
+      >
+        <li
+          @click="toggleCompleted(item)"
+          :key="item.id"
+          class="static-class, priority"
+          :class="{
+            strikeout: item.completed}"
+          >
+          {{ item.label }}
+        </li>
+      <button
         :key="item.id"
-        class="static-class, priority"
-        :class="{
-          strikeout: item.completed}"
-        >
-        {{ item.label }}
+        @click="deleteThis(itemsHighPriority,item.key)"
+        > Delete This </button>
       </li>
     </ul>
     <ul class="NormalPriority">
       <li
         v-for="(item, index) in itemsNormalPriority"
+      >
+        <li
+          @click="toggleCompleted(item)"
+          :key="item.id"
+          class="static-class"
+          :class="{
+            strikeout: item.completed}"
+          >
+          {{ item.label }}
+        </li>
+      <button
         :key="item.id"
-        class="static-class"
-        :class="{
-          strikeout: item.completed}"
-        >
-        {{ item.label }}
+        @click="deleteThis(itemsNormalPriority,item.key)"
+        > Delete This </button>
       </li>
     </ul>
   </div>
